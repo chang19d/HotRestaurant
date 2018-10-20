66a7c6c63847fcaf40d03a7a7f8f6c15470dc71b
Diloretodg <diloretodg74122@gmail.com>
2018-10-20 11:23:51 -0400

    set up routs

--- server.js
+++ server.js
@@ -18,3 +18,26 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
   
+
+app.get("/", function(req, res) {
+// res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+    
+app.get("/tables", function(req, res) {
+// res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+// res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(characters);
+});
+
+app.get("/api/reserved", function(req, res) {
+    return res.json(characters);
+});
